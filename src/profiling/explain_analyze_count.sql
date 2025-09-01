55ab29f0730235b3387a4c114479b38baeab3528
Harsh Bajwa <90355783+harshsbajwa@users.noreply.github.com>
2025-08-31 23:10:00 -0400

    (profiling) explain analyze statement, highest production query

---
+++ src/profiling/explain_analyze_count.sql
@@ -0,0 +1,2 @@
+EXPLAIN ANALYZE SELECT count(*)
+    FROM 'https://github.com/bnokoro/Data-Science/raw/master/countries%20of%20the%20world.csv';
---
+++ src/profiling/profile_highest_production_query.sql
@@ -0,0 +1,19 @@
+PRAGMA enable_profiling;
+SET profiling_mode = 'detailed';
+SET profiling_output = 'profile_highest_production_query.txt';
+SET enable_profiling = 'query_tree';
+
+WITH per_hour AS (
+    SELECT system_id,
+           date_trunc('hour', read_on) AS read_on,
+           avg(power) / 1000 AS kWh
+    FROM readings
+    GROUP BY ALL
+)
+SELECT name,
+       max(kWh),
+       arg_max(read_on, kWh) AS 'Read on'
+    FROM per_hour
+    JOIN systems s ON s.id = per_hour.system_id
+    WHERE system_id = 34
+    GROUP by s.name;
